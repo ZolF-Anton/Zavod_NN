04e54f009b28a3cb1d0a680d34f9481ed56597be
ZolF-werk <zolf@bk.ru>
2021-12-20 17:41:24 +0300

    slider_img

--- src/components/CatalogList/Partlist.jsx
+++ src/components/CatalogList/Partlist.jsx
@@ -1,23 +1,25 @@
 import React from 'react';
 import Part from './Part';
 import Preloader from './preloader';
-import { parts_item } from './mat.module.css';
+import { parts_item, parts_container } from './mat.module.css';
 
 function Partlist(props) {
     const { parts, errorLoad } = props;
 
     return (
-        <div className={parts_item}>
-            {errorLoad ? (
-                <>
-                    <Preloader />
-                    <h4>Nothing...</h4>
-                </>
-            ) : (
-                parts.map((part) => {
-                    return <Part key={part.id} id={part.id} {...part.fields} />;
-                })
-            )}
+        <div className={parts_container}>
+            <div className={parts_item}>
+                {errorLoad ? (
+                    <>
+                        <Preloader />
+                        <h4>Nothing...</h4>
+                    </>
+                ) : (
+                    parts.map((part) => {
+                        return <Part key={part.id} id={part.id} {...part.fields} />;
+                    })
+                )}
+            </div>
         </div>
     );
 }
---
+++ src/components/js/slider_OST.js
@@ -0,0 +1,82 @@
+// Own slider by OstKost
+function nextSlide(up = true) {
+    const slider = document.querySelector('.apparatus__slider');
+    const slides = slider.childNodes;
+    const sliderWidth = slider.offsetWidth;
+    const sliderFullWidth = slider.scrollWidth;
+    const sliderScrolled = slider.scrollLeft;
+    const itemWidth = sliderFullWidth / slides.length;
+    const maxItems = Math.round(sliderWidth / itemWidth);
+    const maxScroll = (slides.length - maxItems) * itemWidth;
+    let left = 0;
+    if (up) {
+        left = sliderScrolled >= maxScroll ? 0 : sliderScrolled + itemWidth;
+    } else {
+        left = sliderScrolled <= 0 ? maxScroll : sliderScrolled - itemWidth;
+    }
+    slider.scrollTo({
+        left,
+        behavior: 'smooth',
+    });
+}
+
+function initApparatusSlider() {
+    let interval = null;
+    const machines = (apparatus && apparatus.records) || [];
+
+    const slider = document.querySelector('.apparatus__slider');
+    if (!slider) return;
+    slider.addEventListener('mouseover', stopSlider);
+    slider.addEventListener('mouseout', startSlider);
+
+    const controls = document.querySelectorAll('.apparatus__control-btn');
+    controls[0].addEventListener('click', () => nextSlide(false));
+    controls[1].addEventListener('click', () => nextSlide(true));
+    controls.forEach((btn) => {
+        btn.addEventListener('mouseover', stopSlider);
+        btn.addEventListener('mouseout', startSlider);
+    });
+
+    function render() {
+        machines.forEach((machine) => {
+            const { Photo, Name } = machine.fields || {};
+            const hasImage = Array.isArray(Photo) && Photo.length > 0;
+            const imageUrl = hasImage ? Photo[0].url : 'img/coffgear.png'; // сделать запасную фотку если фото нет
+            const sliderItem = document.createElement('li');
+            sliderItem.classList.toggle('apparatus__slider-item', true);
+
+            const apparatEl = document.createElement('div');
+            apparatEl.classList.toggle('apparat', true);
+
+            const apparatImageEl = document.createElement('div');
+            apparatImageEl.classList.toggle('apparat__image', true);
+            apparatImageEl.style.backgroundImage = `url(${imageUrl})`;
+
+            const apparatNameEl = document.createElement('div');
+            apparatNameEl.classList.toggle('apparat__name', true);
+            apparatNameEl.innerText = Name;
+
+            const apparatBtnEl = document.createElement('div');
+            apparatBtnEl.classList.toggle('apparat__btn', true);
+            apparatBtnEl.innerText = 'Подробнее';
+            apparatEl.appendChild(apparatImageEl);
+            apparatEl.appendChild(apparatNameEl);
+            apparatEl.appendChild(apparatBtnEl);
+            sliderItem.appendChild(apparatEl);
+            slider.appendChild(sliderItem);
+        });
+        sliderGetAppID();
+    }
+
+    function startSlider() {
+        interval = setInterval(() => {
+            nextSlide();
+        }, 5000);
+    }
+    function stopSlider() {
+        clearInterval(interval);
+    }
+
+    render();
+    startSlider();
+}

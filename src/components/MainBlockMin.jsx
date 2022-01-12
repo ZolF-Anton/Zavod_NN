26ac96b64019650a7129e300addc548c5c1ed504
ZolF-werk <zolf@bk.ru>
2022-01-12 17:35:08 +0300

    catalog-fix

--- src/components/MainBlockMin.jsx
+++ src/components/MainBlockMin.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
-import {} from './mainblockmin.module.scss';
+import {
+    main_block_min,
+    main_block_min__title,
+    main_block_min__subtitle,
+} from './mainblockmin.module.scss';
 
 const MainBlockMin = () => {
     return (
-        <section>
-            <h1>Завод автомобильных глушителей «КД» и «ТДГ» </h1>
-            <p>Работаем с 343 года нашей эры</p>
-            <p>ставили глушаки ещё римским легионерам!</p>
+        <section className={main_block_min}>
+            <h1 className={main_block_min__title}>Завод автомобильных глушителей «КД» и «ТДГ» </h1>
+            <p className={main_block_min__subtitle}>Работаем с 343 года нашей эры</p>
+            <p className={main_block_min__subtitle}>ставили глушаки ещё римским легионерам!</p>
         </section>
     );
 };
